768c3bf70c4b1cb4882fc9e79c07819f2260ec55
David Joshua <davidjosh_87@yahoo.com>
2018-05-23 13:12:35 +0700

    edit and uninstall bcrypt to bcryptjs

--- routes/categories.js
+++ routes/categories.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
-const {categoryCreate, categoryReadAll} = require('../controllers/category.controller');
+const {
+  categoryCreate,
+  categoryReadAll
+} = require('../controllers/category.controller');
 
 router.get('/', categoryReadAll);
 router.post('/', categoryCreate)
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {userCreate, userRead, userReadAll, userUpdate, userDelete} = require('../controllers/user.controller')
-const {isAdmin, authorization} = require('../middlewares/auth')
+const {
+  userCreate,
+  userRead,
+  userReadAll,
+  userUpdate,
+  userDelete
+} = require('../controllers/user.controller')
+const {
+  isAdmin,
+  authorization
+} = require('../middlewares/auth')
 
 /* GET users listing. */
 router.get('/', isAdmin, userReadAll);
